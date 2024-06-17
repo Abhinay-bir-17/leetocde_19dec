06becfd83634e22c7cad554add3ccb882f8a4b28
Abhinay-bir-17 <abhinaybirsingh10@gmail.com>
2024-06-17 23:13:11 +0530

    2nd day, lets goo

--- 42_nonDecreasingArray_leetcode.cpp
+++ 42_nonDecreasingArray_leetcode.cpp
@@ -1,3 +1,31 @@
+// my optimized soln of 17th june 24
+class Solution {
+public:
+	bool checkPossibility(vector<int>& ar) {
+		int n = ar.size();
+		if (n <= 2)return true;
+		int cnt = 0;
+		if (ar[0] > ar[1]) { ar[0] = ar[1]; cnt += 1;}
+		for (int i = 1; i < n - 1; i++) {
+			if (ar[i] < ar[i - 1]) {
+				if (ar[i + 1] < ar[i])return false;/*if 3 els r in striclty
+				decreasin order*/
+				else if (ar[i + 1] >= ar[i - 1]) {ar[i] = ar[i - 1]; cnt += 1;}
+				else if (ar[i + 1] < ar[i - 1]) {
+					if (ar[i] >= ar[i - 2])ar[i - 1] = ar[i];// decrease
+					// above line is for -1 4 2 3
+					else ar[i] = ar[i - 1]; cnt += 1;// increase, ex:3 4 2 3
+					// so increase 2 to 4
+				}
+			}
+			if (cnt > 1)return false;
+		}
+		if (ar[n - 1] < ar[n - 2])cnt += 1;
+		if (cnt > 1)return false; return true;
+	}
+};
+
+//
 class Solution {
 public:
 	bool checkPossibility(vector<int>& ar) {
@@ -45,3 +73,4 @@ public:
 		} return true;
 	}
 };
+//
